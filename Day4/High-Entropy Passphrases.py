f5f191d86157a91afb1b80bbdb30cb92547a3f66
Draeriel <32552483+Draeriel@users.noreply.github.com>
2017-12-08 11:45:59 +0100

    Advent of Code, Day 4, part 1

---
+++ Day4/High-Entropy Passphrases.py
@@ -0,0 +1,35 @@
+def validator(rutaDeAcceso, password):
+	fichero = open(rutaDeAcceso, 'r')
+	lineas = 0
+	invalido = 0
+	for linea in fichero:
+		lineas += 1
+		colector = []
+		conjunto = ""
+		erroneo = 0
+		for letra in linea:
+			if letra.isalpha():
+				conjunto += letra
+			else:
+				if conjunto not in colector:
+					colector.append(conjunto)
+				else:
+					if erroneo == 0:
+						invalido +=1
+						erroneo += 1	
+				conjunto = ""
+		if len(conjunto) > 0:
+			if conjunto not in colector:
+				colector.append(conjunto)
+			else:
+				if erroneo == 0:
+						invalido +=1
+						erroneo += 1		
+	valido = lineas - invalido
+	return valido
+
+
+
+rutaDeAcceso = "casosTest.txt"
+password = []
+print(validator(rutaDeAcceso, password))
